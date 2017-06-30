e3d64a7fdedcb2aef61be945604c9b2768f8b483
MatiasM <matiasmendoza1994@gmail.com>
2017-06-30 20:49:05 -0300

    Se agrega un admincontroller con un metodo para crear usuarios administradores, ademas se agrega la configuracion del dispatcher servlet para agregar la ruta admin/crearadmin y su vista

--- src/java/Controller/AdminController.java
+++ src/java/Controller/AdminController.java
@@ -5,10 +5,37 @@
  */
 package Controller;
 
+import Entity.Usuario;
+import Service.RolService;
+import Service.UsuarioService;
+import javax.servlet.http.HttpServletRequest;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
     
-    
+    @RequestMapping(value="admin/crearadmin", method = RequestMethod.GET)
+    public ModelAndView crearAdmin(){
+        return new ModelAndView("admin/crearadmin");
+        
+    }@RequestMapping(value="admin/crearadmin", method = RequestMethod.POST)
+    public ModelAndView crearAdminPOST(HttpServletRequest request){
+        ModelAndView mav = new ModelAndView();
+        String hash1 = BCrypt.hashpw(request.getParameter("password"), BCrypt.gensalt());
+        mav.addObject("nombre", request.getParameter("nombre"));
+        mav.addObject("email", request.getParameter("email"));
+        mav.addObject("password", request.getParameter("password"));
+        mav.addObject("hash1", hash1);
+        Usuario user = new Usuario();
+        user.setNombre(request.getParameter("nombre"));
+        user.setEmail(request.getParameter("email"));
+        user.setContrasena(hash1);
+        UsuarioService.add(user, RolService.getByName("administrador"));
+        mav.setViewName("pruebas");
+        return mav;
+    }
 }
